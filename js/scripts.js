7f7e45daf1384dfd1dbda2684c47566edf39bb42
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-12-09 20:02:02 -0800

    add AllTopping property to Toppings class that concats RegTopping and FunTopping.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function Pizza() {
   this.size = Size.Medium;
-  this.toppings = Toppings.;
+  this.sauces = 0;
+  this.toppings = Array(Toppings.AllTopping.length).fill(0);
   this.cost = 0;
 }
 
@@ -18,6 +19,7 @@ class Toppings {
   static Sauces = new Toppings(["chocolate ganache", "brown butter caramel", "maple syrup", "lemon curd"], Array(4).fill(0));
   static RegTopping = new Toppings(["M&M's", "caramel popcorn", "peanut butter cups", "Skittles", "Nerdz", "marshmallows", "mixed berries", "sprinkles", "cookie dough", "whipped cream"], Array(10).fill(.75));
   static FunTopping = new Toppings(["pretzels", "peanuts", "chili flakes", "smoked sea salt", "bacon crumbles", "gold leaf"], [.80, .80, .80, .80, 1.50, 3]);
+  static AllTopping = new Toppings((Toppings.RegTopping.toppings).concat(Toppings.FunTopping.toppings), (Toppings.RegTopping.prices).concat(Toppings.FunTopping.prices));T
 
   constructor(toppings, prices) {
     this.toppings = toppings;
@@ -36,12 +38,6 @@ Pizza.prototype.addTopping = function() {
 Pizza.prototype.addCost = function() {
 
 };
-
-function Toppings() {
-  const toppingType = [];
-  this.sauce = [];
-  this.halfTopping = false;
-}
 
 Toppings.prototype.chooseTopping = function() {
 
