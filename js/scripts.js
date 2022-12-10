ad11e8677889b4e5a0fbf6dd2815a949464af790
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-12-09 16:23:08 -0800

    fill out rough draft of pizza object, topping object, and methods in js.

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+function Pizza() {
+  this.size = [0, 1, 2];
+  this.toppings = {};
+  this.cost = 0;
+}
+
+Pizza.prototype.chooseSize = function() {
+
+};
+
+Pizza.prototype.addTopping = function() {
+
+};
+
+Pizza.prototype.addCost = function() {
+
+};
+
+function Toppings() {
+  this.toppingType = [];
+  this.sauce = [];
+  this.halfTopping = false;
+}
+
+Toppings.prototype.chooseTopping = function() {
+  
+}
+
+
+
+
+
+
+
