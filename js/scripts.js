377f175c84e5bc594b708c8fbddf0d6805d0c3db
Ariel Doyle <ariel.rose.garrett@gmail.com>
2022-12-09 18:53:13 -0800

    attempt to test using js classes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
 function Pizza() {
-  this.size = [0, 1, 2];
-  this.toppings = {};
+  this.size = Size.Medium;
+  this.toppings = Toppings.;
   this.cost = 0;
+}
+
+class Size {
+  static Small = new Size("small");
+  static Medium = new Size("medium");
+  static Large = new Size("large");
+
+  constructor(size) {
+    this.size = size;
+  }
+}
+
+class Toppings {
+  static Sauces = new Toppings(["chocolate ganache", "brown butter caramel", "maple syrup", "lemon curd"], Array(4).fill(0));
+  static RegTopping = new Toppings(["M&M's", "caramel popcorn", "peanut butter cups", "Skittles", "Nerdz", "marshmallows", "mixed berries", "sprinkles", "cookie dough", "whipped cream"], Array(10).fill(.75));
+  static FunTopping = new Toppings(["pretzels", "chili flakes", "smoked sea salt", "bacon crumbles", "gold leaf"], [.80, .80, .80, 1.50, 3]);
+
+  constructor(toppings,prices) {
+    this.toppings = toppings;
+    this.prices = prices;
+  }
 }
 
 Pizza.prototype.chooseSize = function() {
@@ -17,13 +38,13 @@ Pizza.prototype.addCost = function() {
 };
 
 function Toppings() {
-  this.toppingType = [];
+  const toppingType = [];
   this.sauce = [];
   this.halfTopping = false;
 }
 
 Toppings.prototype.chooseTopping = function() {
-  
+
 }
 
 
